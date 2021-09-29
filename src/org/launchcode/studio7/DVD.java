bac71df0ec57856d8ed31bc0bc37e6088b0ba040
Tobias Sol <insightfortheblind@gmail.com>
2021-09-28 20:52:47 -0500

    Full code

--- src/org/launchcode/studio7/DVD.java
+++ src/org/launchcode/studio7/DVD.java
@@ -1,6 +1,19 @@
 package org.launchcode.studio7;
 
-public class DVD {
+public class DVD extends BaseDisc implements  OpticalDisc {
+    public DVD(String aName, int maxCapacity, String aType, int someUsedCapacity) {
+        super(aName, maxCapacity, aType, someUsedCapacity);
+    }
+
+    @Override
+    public void spinDisc() {
+        System.out.println("A DVD spins at a rate of 570 - 1600 rpm.");
+    }
+
+    @Override
+    public void readData() {
+        System.out.println("Data has been read. Super cool puppy pictures and Lysol for the bathroom found inside.");
+    }
 
     // TODO: Implement your custom interface.
 
--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -3,10 +3,22 @@ package org.launchcode.studio7;
 public class Main {
 
     public static void main(String[] args){
-
+        CD cd = new CD("Hybrid Theory", 700, "CD-R", 350);
+        DVD dvd = new DVD("Little Mermaid", 4700, "DVD-R", 1450);
         // TODO: Declare and initialize a CD and a DVD object.
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
+        cd.spinDisc();
+        dvd.spinDisc();
+
+        cd.readData();
+        dvd.readData();
+
+        System.out.println(cd.writeData(275));
+        System.out.println(dvd.writeData(8000));
+
+        System.out.println(cd.diskInfo());
+        System.out.println(dvd.diskInfo());
 
     }
 }
